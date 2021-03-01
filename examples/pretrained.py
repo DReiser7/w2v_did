c8ad96b4391b81420df655da54fdfc5d6a9d7547
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-03-01 18:30:36 +0100

    change model because other one did not work

--- examples/pretrained.py
+++ examples/pretrained.py
@@ -1,11 +1,11 @@
-# !pip install fairseq TODO does not work for me
+# !pip install fairseq
 # !pip install torch
 import torch
 import fairseq
 
 if __name__ == "__main__":
 
-    cp_path = '../models/wav2vec_vox_960h_pl.pt' # TODO: download https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_vox_960h_pl.pt
+    cp_path = '../models/wav2vec_large.pt' # TODO: https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_large.pt
     model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([cp_path])
     model = model[0]
     model.eval()
