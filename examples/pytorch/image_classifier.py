c44451578adcad13010868bbcd742601dabe71c1
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-03-07 12:03:33 +0100

    add some info evaluation to the image classifier

--- examples/pytorch/image_classifier.py
+++ examples/pytorch/image_classifier.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import os
 
+
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
@@ -102,3 +103,50 @@ if __name__ == "__main__":
     # print images
     imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+    #laod back saved Model
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+
+    # Okay, now let us see what the neural network thinks these examples above are:
+    outputs = net(images)
+
+    # The outputs are energies for the 10 classes. The higher the energy for a class,
+    # the more the network thinks that the image is of the particular class.
+    # So, let’s get the index of the highest energy:
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                  for j in range(4)))
+
+    # The results seem pretty good.
+    # Let us look at how the network performs on the whole dataset.
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+    # Hmmm, what are the classes that performed well, and the classes that did not perform well:
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
