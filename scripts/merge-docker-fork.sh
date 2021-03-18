3e67c29de9d7645514f4afbdcda06b34558ac785
Synoon <lastsouloe@gmail.com>
2021-03-18 17:41:24 +0100

    pycache to gitignore add clone and stuff to merge file for automatic updated CI/CD

--- scripts/merge-docker-fork.sh
+++ scripts/merge-docker-fork.sh
@@ -1,4 +1,20 @@
-cd ../../f_w2v_did/w2v_did
+# enter into script folder with git bash and run over " start merge-docker-fork.sh "
+
+cd ../../
+
+if [ ! -d "/f_w2v_did" ]
+then
+   mkdir f_w2v_did
+   cd f_w2v_did
+   git clone https://github.com/Synoon/w2v_did.git
+   git remote add upstream https://github.com/DReiser7/w2v_did.git
+fi
+
+
+cd f_w2v_did/w2v_did
 git fetch upstream
 git merge upstream/master
 git push
+
+echo "type something "
+input
