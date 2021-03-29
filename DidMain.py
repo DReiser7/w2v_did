2be33edfad747377a5239ee9678db14bd95a95f2
Synoon <lastsouloe@gmail.com>
2021-03-29 14:32:07 +0200

    wandb parallel processing

--- DidMain.py
+++ DidMain.py
@@ -46,6 +46,17 @@ def train(gpu, args):
     )
     torch.cuda.set_device(gpu)
 
+    # Initialize a new wandb run
+    wb_name = datetime.now().strftime("w2v_did " + "_%Y%m%d-%H%M%S")
+    if rank == 0:
+        wandb.init(project='w2v_did', config=did_config, entity='ba-reisedomfiviapas',
+                   name=wb_name)
+        # Config is a variable that holds and saves hyperparameters and inputs
+        config = wandb.config
+        print_Config()
+
+    wandb.init(group=wb_name)
+
     # build train data
     csv_path_train = config.data['train_dataset'] + 'metadata.csv'  # file_path_train = './data/dev/segmented/'
     train_set = DidDataset(csv_path_train, config.data['train_dataset'])
@@ -127,24 +138,22 @@ def train(gpu, args):
                             wandb=wandb,
                             loss_function=loss_function)
 
-    wandb.watch(model)
+    if rank == 0:
+        wandb.watch(model)
 
     for epoch in range(config.general['epochs']):
         closs = runner.train(train_loader=train_loader,
                              epoch=epoch,
                              log_interval=config.general['log_interval'])
-        wandb.log({"loss": closs / (len(train_loader.dataset) / config.batch_size)})
+        if rank == 0:
+            wandb.log({"loss": closs / (len(train_loader.dataset) / config.batch_size)})
 
         if epoch % config.general['model_save_interval'] == 0:  # test and save model every n epochs
             accuracy = runner.test(test_loader=test_loader)
-            wandb.log({"accuracy": accuracy})
-        #     model_path = './models/did_model_epoch_' + str(epoch) + '.pt'
-        #     print("Saving model to " + model_path)
-        #     torch.save(model.state_dict(), model_path)
+            if rank == 0:
+                wandb.log({"accuracy": accuracy})
 
         scheduler.step()
-
-    print('Finished Training')
 
 
 if __name__ == "__main__":
@@ -164,115 +173,16 @@ if __name__ == "__main__":
     # get device on which training should run
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Initialize a new wandb run
-    wandb.init(project='w2v_did', config=did_config, entity='ba-reisedomfiviapas',
-               name=datetime.now().strftime("w2v_did " + "_%Y%m%d-%H%M%S"))
-    # Config is a variable that holds and saves hyperparameters and inputs
-    config = wandb.config
-    print_Config()
-
     # Using more than one GPU
     if torch.cuda.device_count() > 1:
         device_count = torch.cuda.device_count()
         args.world_size = device_count * args.nodes
         args.gpus = device_count
-        args.config = config
+        args.config = did_config
         os.environ['MASTER_ADDR'] = 'localhost'
         os.environ['MASTER_PORT'] = '8711'
         print("set env vars")
         print("Using:", device_count, "GPUs!")
         mp.spawn(train, nprocs=args.gpus, args=(args,))
-        # config_defaults["batch_size"] = config_defaults["batch_size"] * device_count
-        # print("Multiplying batch * GPUs new batch_size=", config_defaults["batch_size"])
 
-    # # define params for data loaders
-    # kwargs = {'num_workers': config.general['num_workers'],
-    #           'pin_memory': True} if device == 'cuda' else {}  # needed for using datasets on gpu
-    #
-    # # build train data
-    # csv_path_train = config.data['train_dataset'] + 'metadata.csv'  # file_path_train = './data/dev/segmented/'
-    # train_set = DidDataset(csv_path_train, config.data['train_dataset'])
-    # print("Train set size: " + str(len(train_set)))
-    #
-    # # build test data
-    # csv_path_test = config.data['test_dataset'] + 'metadata.csv'  # file_path_test = './data/dev/segmented/'
-    # test_set = DidDataset(csv_path_test, config.data['test_dataset'])
-    # print("Test set size: " + str(len(test_set)))
-    #
-    # # build data loaders
-    # train_loader = torch.utils.data.DataLoader(train_set,
-    #                                            batch_size=config.data['batch_size'],
-    #                                            shuffle=config.data['shuffle'],
-    #                                            **kwargs)
-    # test_loader = torch.utils.data.DataLoader(test_set,
-    #                                           batch_size=config.data['batch_size'],
-    #                                           shuffle=config.data['shuffle'],
-    #                                           **kwargs)
-    #
-    # # Loss Function and fitting exponential normalizing function
-    # if config.general['loss_function'] == 'nllLoss':
-    #     loss_function = F.nll_loss
-    #     exp_norm_func = F.log_softmax
-    # else:
-    #     raise SystemExit("you must specify loss_function for " + config.general['loss_function'])
-    #
-    # # Using more than one GPU
-    # if torch.cuda.device_count() > 1:
-    #     print("Wrapping loss_function with DataParallelCriterion")
-    #     loss_function = loss_function
-    #
-    # # create our own model with classifier on top of fairseq's xlsr_53_56k.pt
-    # model = DidModel(model_path=config.model['model_location'],
-    #                  num_classes=config.model['num_classes'],
-    #                  exp_norm_func=exp_norm_func,
-    #                  freeze_fairseq=config.model['freeze_fairseq'])
-    #
-    # # Using more than one GPU
-    # if torch.cuda.device_count() > 1:
-    #     print("Wrapping model with DataParallel")
-    #     model = DataParallel(model)
-    #
-    # # Optimizer
-    # print('optimizer_params:')
-    # if config.general['optimizer'] == 'adam':
-    #     print('  lr: ' + str(config.optimizers[config.general['optimizer']]['lr']) + ', weight_decay: ' + str(
-    #         config.optimizers[config.general['optimizer']]['weight_decay']))
-    #     optimizer = optim.Adam(model.parameters(),
-    #                            lr=config.optimizers[config.general['optimizer']]['lr'],
-    #                            weight_decay=config.optimizers[config.general['optimizer']]['weight_decay'])
-    # else:
-    #     raise SystemExit("you must specify optimizer for " + config.general['optimizer'])
-    #
-    # # Scheduler
-    # print('scheduler_params:')
-    # print('  step_size: ' + str(config.scheduler['step_size']) + ', gamma: ' + str(config.scheduler['gamma']))
-    # scheduler = optim.lr_scheduler.StepLR(optimizer,
-    #                                       step_size=config.scheduler['step_size'],
-    #                                       gamma=config.scheduler['gamma'])
-    #
-    # # create runner for training and testing
-    # runner = DidModelRunner(device=device,
-    #                         model=model,
-    #                         optimizer=optimizer,
-    #                         scheduler=scheduler,
-    #                         wandb=wandb,
-    #                         loss_function=loss_function)
-    #
-    # wandb.watch(model)
-    #
-    # for epoch in range(config.general['epochs']):
-    #     closs = runner.train(train_loader=train_loader,
-    #                          epoch=epoch,
-    #                          log_interval=config.general['log_interval'])
-    #     wandb.log({"loss": closs / (len(train_loader.dataset) / config.batch_size)})
-    #
-    #     if epoch % config.general['model_save_interval'] == 0:  # test and save model every n epochs
-    #         accuracy = runner.test(test_loader=test_loader)
-    #         wandb.log({"accuracy": accuracy})
-    #     #     model_path = './models/did_model_epoch_' + str(epoch) + '.pt'
-    #     #     print("Saving model to " + model_path)
-    #     #     torch.save(model.state_dict(), model_path)
-    #
-    #     scheduler.step()
-    #
-    # print('Finished Training')
+    print('Finished Training')
