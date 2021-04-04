e64950e9cbba6f9db6a99d49f20af78502e27e60
Synoon <lastsouloe@gmail.com>
2021-04-04 13:37:32 +0200

    performance logs

--- DidModelRunner.py
+++ DidModelRunner.py
@@ -28,10 +28,12 @@ class DidModelRunner:
             z = time.time()
             output = self.model(data)['normalized']
             self.wandb.log({"model_calc_duration": (time.time() - z)})
+            z = time.time()
             loss = self.loss_function(output, target)
             loss.backward()
             self.optimizer.step()
-            self.wandb.log({"batch loss": loss.detach().item()})
+            self.wandb.log({"batch_loss_calc_duration": (time.time() - z)})
+            self.wandb.log({"batch_loss": loss.detach().item()})
             closs = closs + loss.detach().item()
             if batch_idx % log_interval == 0:  # print training stats
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch,
@@ -52,9 +54,13 @@ class DidModelRunner:
             for batch_idx, (data, target) in enumerate(test_loader):
                 data = data.to(self.device)
                 target = target.to(self.device)
+                z = time.time()
                 output = self.model(data)
+                self.wandb.log({"eval_model_calc_duration": (time.time() - z)})
+                z = time.time()
                 loss = self.loss_function(output['normalized'], target)  # the loss functions expects a batchSizex5 input
                 vloss = vloss + loss.detach().item()
+                self.wandb.log({"eval_loss_calc_duration": (time.time() - z)})
                 pred = output['x'].max(1)[1]  # get the index of the max log-probability
                 correct += pred.eq(target).cpu().sum().item()
                 if batch_idx % log_interval == 0:  # print training stats
