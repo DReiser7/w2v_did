e9db672c830bd13700d1c5e040cf719492f07ca9
Synoon <lastsouloe@gmail.com>
2021-04-10 14:25:12 +0200

    data path to arguments

---
+++ DidTrainer.py
@@ -0,0 +1,48 @@
+from typing import Any, Dict, Union
+
+import torch
+from torch import nn
+from transformers import Trainer
+
+
+class DidTrainer(Trainer):
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        """
+        Perform a training step on a batch of inputs.
+
+        Subclass and override to inject custom behavior.
+
+        Args:
+            model (:obj:`nn.Module`):
+                The model to train.
+            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+
+        Return:
+            :obj:`torch.Tensor`: The tensor with training loss on this batch.
+        """
+
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+        loss = self.compute_loss(model, inputs)
+
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        loss.backward()
+
+        return loss.detach()
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        # labels = inputs.pop("labels").to('cuda')
+        labels = inputs['labels'].to('cuda')
+        outputs = model(**inputs)  # torch.Size([32, 5])
+        loss_fct = torch.nn.CrossEntropyLoss()
+        loss = loss_fct(outputs['logits'],
+                        labels.argmax(-1).long())
+
+        return (loss, outputs) if return_outputs else loss
+
