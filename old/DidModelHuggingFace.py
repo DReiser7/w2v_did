b2d6fa0f7da0d497c2cb75cc6ccc0d435873531d
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-04-10 13:08:18 +0200

    try to change our old model

--- old/DidModelHuggingFace.py
+++ old/DidModelHuggingFace.py
@@ -28,16 +28,16 @@ class DidModelHuggingFace(nn.Module):
         for params in self.model.feature_projection.parameters():
             params.requires_grad = False
 
-        self.classifier_layer = nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Linear(1024, 1024),
-            nn.LeakyReLU(),
-            nn.Linear(1024, 1024),
-            nn.Sigmoid(),
-            nn.Linear(1024, 1024),
-            nn.Sigmoid(),
-            nn.Linear(1024, num_classes)
-        )
+        self.inner = 128
+        self.features = 999
+
+        self.leakyReLu = nn.LeakyReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.fc1 = nn.Linear(1024, self.inner)
+        self.fc2 = nn.Linear(self.inner * self.features, 1024)
+        self.fc3 = nn.Linear(1024, 1024)
+        self.fc4 = nn.Linear(1024, num_classes)
 
         self.exp_norm_func = exp_norm_func
 
@@ -48,12 +48,16 @@ class DidModelHuggingFace(nn.Module):
             self.dfs_freeze(child)
 
     def forward(self, source):
-        x = self.model(source)
+        outputs = self.model(source)
 
         # reduce dimension with mean
-        x_reduced = torch.mean(x.last_hidden_state, -2)
-
-        x = self.classifier_layer(x_reduced)
+        x = self.fc1(outputs[0])
+        x = self.leakyReLu(x)
+        x = self.fc2(x.view(-1, self.inner * self.features))
+        x = self.leakyReLu(x)
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+        x = self.fc4(x)
 
         normalized = self.exp_norm_func(x, dim=1)
 
