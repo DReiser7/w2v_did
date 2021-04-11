7b90674e31600478f816bb955d10410af137f4a9
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-04-11 13:27:24 +0200

    simplify training

--- DidTrainer.py
+++ DidTrainer.py
@@ -6,43 +6,41 @@ from transformers import Trainer
 
 
 class DidTrainer(Trainer):
-    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
-        """
-        Perform a training step on a batch of inputs.
-
-        Subclass and override to inject custom behavior.
-
-        Args:
-            model (:obj:`nn.Module`):
-                The model to train.
-            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
-                The inputs and targets of the model.
-
-                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
-                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
-
-        Return:
-            :obj:`torch.Tensor`: The tensor with training loss on this batch.
-        """
-
-        model.train()
-        inputs = self._prepare_inputs(inputs)
-        loss = self.compute_loss(model, inputs)
-
-        if self.args.gradient_accumulation_steps > 1:
-            loss = loss / self.args.gradient_accumulation_steps
-
-        loss.backward()
-
-        return loss.detach()
+    # def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+    #     """
+    #     Perform a training step on a batch of inputs.
+    #
+    #     Subclass and override to inject custom behavior.
+    #
+    #     Args:
+    #         model (:obj:`nn.Module`):
+    #             The model to train.
+    #         inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+    #             The inputs and targets of the model.
+    #
+    #             The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+    #             argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+    #
+    #     Return:
+    #         :obj:`torch.Tensor`: The tensor with training loss on this batch.
+    #     """
+    #
+    #     model.train()
+    #     inputs = self._prepare_inputs(inputs)
+    #     loss = self.compute_loss(model, inputs)
+    #
+    #     if self.args.gradient_accumulation_steps > 1:
+    #         loss = loss / self.args.gradient_accumulation_steps
+    #
+    #     loss.backward()
+    #
+    #     return loss.detach()
 
     def compute_loss(self, model, inputs, return_outputs=False):
         # labels = inputs.pop("labels").to('cuda')
-        labels = inputs['labels'].to('cuda')
+        labels = inputs['labels'].to('cpu')
         outputs = model(**inputs)  # torch.Size([32, 5])
         loss_fct = torch.nn.CrossEntropyLoss()
-        loss = loss_fct(outputs['logits'],
-                        labels.argmax(-1).long())
+        loss = loss_fct(outputs['logits'], labels)
 
         return (loss, outputs) if return_outputs else loss
-
