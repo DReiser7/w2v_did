c219e221ef6acd8468a04c61b465653aafd0313c
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-04-23 16:49:41 +0200

    make audioconverter parallel

--- AudioConverter.py
+++ AudioConverter.py
@@ -1,6 +1,20 @@
 import sys
 from pathlib import Path
 from pydub import AudioSegment
+import multiprocessing as mp
+
+
+def convert_file(path, input_dir, output_dir, input_format, output_format):
+    print('converting ' + str(path) + ' from ' + input_format + ' to ' + output_format)
+    file = AudioSegment.from_file(path)
+    subdir = str(path.parent).replace('\\', '/').replace(input_dir, '')
+    path_file = output_dir + subdir + '/' + path.stem + '.' + output_format
+
+    dir_create = Path(output_dir + subdir)
+    dir_create.mkdir(parents=True, exist_ok=True)
+
+    file.export(path_file, format=output_format)
+
 
 # python AudioConverter.py "/input" "/output" "m4a" "mp3"
 if __name__ == "__main__":
@@ -11,15 +25,9 @@ if __name__ == "__main__":
 
     path_list = Path(input_dir).glob('**/*.' + input_format)
 
-    for path in path_list:
-        print('converting ' + str(path) + ' from ' + input_format + ' to ' + output_format)
-        file = AudioSegment.from_file(path)
-        subdir = str(path.parent).replace('\\', '/').replace(input_dir, '')
-        path_file = output_dir + subdir + '/' + path.stem + '.' + output_format
-
-        dir_create = Path(output_dir + subdir)
-        dir_create.mkdir(parents=True, exist_ok=True)
-
-        file.export(path_file, format=output_format)
+    print('parallelizing level: ' + str(mp.cpu_count()))
+    pool = mp.Pool(mp.cpu_count())
+    pool.starmap(convert_file, [(path, input_dir, output_dir, input_format, output_format) for path in path_list])
+    pool.close()
 
     print('finished converting!')
