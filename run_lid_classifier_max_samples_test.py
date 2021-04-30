1f8e09cf2cba736ed5152a5cda747e87eed090dd
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-04-30 15:23:56 +0200

    remove dynamic input variants

--- run_lid_classifier_max_samples_test.py
+++ run_lid_classifier_max_samples_test.py
@@ -464,12 +464,12 @@ if __name__ == "__main__":
     # We now keep distinct sets of args, for a cleaner separation of concerns.
 
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
-    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+    # if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
-        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
-    else:
-        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    # else:
+    #     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
     number_of_samples = int(sys.argv[2])
 
