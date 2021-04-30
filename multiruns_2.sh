265ee4a0d9ec3b6729407dc7fd03eb751e2cfaca
Synoon <lastsouloe@gmail.com>
2021-04-30 15:45:09 +0200

    second run

---
+++ multiruns_2.sh
@@ -0,0 +1,6 @@
+python ./run_lid_classifier_max_samples_test.py "config/config_fiviapas_LID_max_samples_2.json" 250
+python ./run_lid_classifier_max_samples_test.py "config/config_fiviapas_LID_max_samples_2.json" 500
+python ./run_lid_classifier_max_samples_test.py "config/config_fiviapas_LID_max_samples_2.json" 1000
+python ./run_lid_classifier_max_samples_test.py "config/config_fiviapas_LID_max_samples_2.json" 2000
+python ./run_lid_classifier_max_samples_test.py "config/config_fiviapas_LID_max_samples_2.json" 3000
+python ./run_lid_classifier_max_samples_test.py "config/config_fiviapas_LID_max_samples_2.json" 4000
