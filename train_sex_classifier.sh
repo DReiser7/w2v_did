1498c8fda907d89f08f040e65adcc0fcca769f90
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-05-08 16:10:31 +0200

    prepare train sex experiments

---
+++ train_sex_classifier.sh
@@ -0,0 +1,4 @@
+python ./train_classifier_sex.py "config/config_reisedom_german_sex_max_samples.json" 1000
+python ./train_classifier_sex.py "config/config_reisedom_german_sex_max_samples.json" 2000
+python ./train_classifier_sex.py "config/config_reisedom_german_sex_max_samples.json" 3000
+python ./train_classifier_sex.py "config/config_reisedom_german_sex_max_samples.json" 4000
