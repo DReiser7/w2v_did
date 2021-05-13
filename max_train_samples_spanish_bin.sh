ff314dbc0810343c6a3ff4d17bd4fedcac9eccea
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-05-13 07:46:25 +0200

    push spanishj binary

---
+++ max_train_samples_spanish_bin.sh
@@ -0,0 +1,4 @@
+python ./train_classifier_spanish-bin.py "config/config_reisedom_spanish_binary.json" 1000
+python ./train_classifier_spanish-bin.py "config/config_reisedom_spanish_binary.json" 2000
+python ./train_classifier_spanish-bin.py "config/config_reisedom_spanish_binary.json" 3000
+python ./train_classifier_spanish-bin.py "config/config_reisedom_spanish_binary.json" 4000
