a1e73c419e06f1a7cf9c6fa03910c05c83885619
Synoon <lastsouloe@gmail.com>
2021-05-15 19:48:51 +0200

    run 1 continue

--- train_classifier_english_run.sh
+++ train_classifier_english_run.sh
@@ -1,6 +1,2 @@
-python ./train_classifier_english.py "config/config_fiviapas_english_accents.json" 6000
-python ./train_classifier_english.py "config/config_fiviapas_english_accents.json" 5000
-python ./train_classifier_english.py "config/config_fiviapas_english_accents.json" 4000
-python ./train_classifier_english.py "config/config_fiviapas_english_accents.json" 3000
 python ./train_classifier_english.py "config/config_fiviapas_english_accents.json" 2000
 python ./train_classifier_english.py "config/config_fiviapas_english_accents.json" 1000
