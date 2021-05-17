7e3ee4c09b1aecf7224dcd1a8eab00f1604e874a
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-05-17 15:32:51 +0200

    add eval spanish multi to bin

---
+++ eval_spanish_multi-to-bin.sh
@@ -0,0 +1,3 @@
+python ./eval_spanish_multi-to-bin.py "config/config_reisedom_spanish_eval_multi-to-bin.json" "1/4000"
+python ./eval_spanish_multi-to-bin.py "config/config_reisedom_spanish_eval_multi-to-bin.json" "2/4000"
+python ./eval_spanish_multi-to-bin.py "config/config_reisedom_spanish_eval_multi-to-bin.json" "3/4000"
