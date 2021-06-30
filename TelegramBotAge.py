03c493ac344a8a7f38b3183df581e8648c2eda4c
Reiser,Dominique D.R <d.reiser@student.fontys.nl>
2021-06-30 10:57:52 +0200

    add start stop outpüuts

--- TelegramBotAge.py
+++ TelegramBotAge.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         file_info = bot.get_file(message.voice.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
 
-        file_name = message.chat.first_name + message.chat.last_name + str(message.id)
+        file_name = message.chat.first_name + message.chat.last_name + (str(message.id)  if message.id else "_unknown")
         file_ending = ".mp3"
 
         full_path = path + file_name + file_ending
